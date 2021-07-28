b2e666eadf53d143a2d6a417e7ec85e16a6cf099
Vincent Y <vincenzoyee@gmail.com>
2021-07-27 20:33:56 -0600

    added ability for multiple dropdowns using the dropdown-container class

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-function dropdown() {
-    const dropdown_parent = document.querySelector('.dropdown-container');
-
-    children = dropdown_parent.children;
+function dropdown(dropdown_parent) {
+    
+    let children = dropdown_parent.children;
 
     for (let i = 0; i < children.length; i++) {
         children[i].style.display = 'none';
@@ -23,4 +22,8 @@ function dropdown() {
     });
 }
 
-dropdown();
+const all_drop_downs = document.querySelectorAll('.dropdown-container');
+
+for (let i = 0; i < all_drop_downs.length; i++) {
+    dropdown(all_drop_downs[i]);
+}
